8d13449d487693c429c7adcce94cc09d6288224a
sunkyoung <yskkjdh@gmail.com>
2024-03-12 02:25:03 +0900

    setting : 전역예외처리 및 application.yml 생성
    Change-Id: I06241e6d1799c7b903269b04513ced52fb7fe894
    
    Former-commit-id: 0ed9aa73af8b8cb6b17d54a39ddf806a404371f8

---
+++ bada-back/src/main/java/com/bada/badaback/global/config/SecurityConfig.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e0348a28951bcefd99f0c944af508d20522f08d1ca058eeaae60d109500c86b6
+size 1993
---
+++ bada-back/src/main/java/com/bada/badaback/global/config/WebConfig.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:51f28e8cefd9d65802411a42145572e85956f3496d604737fe7a58244b1b2b25
+size 650
