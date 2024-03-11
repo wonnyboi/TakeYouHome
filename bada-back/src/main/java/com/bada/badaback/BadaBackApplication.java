25471d92cf769f2c50b1052ab69c43376dfbbc73
sunkyoung <yskkjdh@gmail.com>
2024-03-12 02:24:37 +0900

    setting : 스프링부트 기본 환경 세팅
    Change-Id: Ie89e4c4e6ba4d23868d6470e15c8980eee96fa63
    
    Former-commit-id: 6030d2069649535fca536537722d5dcb40122577

---
+++ bada-back/src/main/java/com/bada/badaback/BadaBackApplication.java
@@ -0,0 +1,13 @@
+package com.bada.badaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadaBackApplication.class, args);
+	}
+
+}
