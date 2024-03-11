8ddeb3adb6dc9fc5334b49d04bc8979478158f41
sunkyoung <yskkjdh@gmail.com>
2024-03-12 02:25:03 +0900

    setting : 전역예외처리 및 application.yml 생성
    Change-Id: I06241e6d1799c7b903269b04513ced52fb7fe894

---
+++ bada-back/src/main/java/com/bada/badaback/global/config/JpaAuditingConfig.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:afc7a350d507f0be44ba04969a99b1771ebfee483a47a925f187132c56b6d4f9
+size 246
---
+++ bada-back/src/main/java/com/bada/badaback/global/exception/BaseException.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:85414cfb758c0959f672b589e073e55d5684023c5ff6ca68115e994a44c8d3a6
+size 378
