98a018b92e64aff3f278111f00d9f86f5b70065e
sunkyoung <yskkjdh@gmail.com>
2024-03-12 02:24:37 +0900

    setting : 스프링부트 기본 환경 세팅
    Change-Id: Ie89e4c4e6ba4d23868d6470e15c8980eee96fa63

---
+++ bada-back/src/main/java/com/bada/badaback/BadaBackApplication.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:41710085744d4989192710a962d3b91892c70c0210750c51cc6d2ac9711c315c
+size 314
---
+++ bada-back/src/test/java/com/bada/badaback/BadaBackApplicationTests.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d6358f36714b43ba1064ebc2896ff6bd8fd61e644c9a2ac7a167d38fdd06e64f
+size 211
