29243bd3da69764c79cff225d3e99d72b452e964
sunkyoung <yskkjdh@gmail.com>
2024-03-12 02:25:13 +0900

    setting : 테스트코드 작성을 위한 세팅
    Change-Id: Ia5a8739f39f572939f31466d4385c30bd1fe6cf7

---
+++ bada-back/src/test/java/com/bada/badaback/common/ControllerTest.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5cba89e9e1bd59d9bd19a1c25f889d93939659a68a07618dc172ec24f2eb1f2d
+size 926
---
+++ bada-back/src/test/java/com/bada/badaback/common/RepositoryTest.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1d878faf58619a5121cc927b0e557c671a300edb71c4ff1576154f6857012e97
+size 299
