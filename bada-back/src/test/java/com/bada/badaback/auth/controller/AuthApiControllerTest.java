a57b96bbcfe411d7c088bec853b3999a2f26ae2e
sunkyoung <yskkjdh@gmail.com>
2024-03-16 20:27:06 +0900

    test : 아이 회원가입 API 테스트 코드 작성
    Change-Id: I50c6d77b56a61c771de3dfd66c21aae46697e0fb
    
    Former-commit-id: ce83a1b41e20fcb1dee8673d5095a2b40db05b68

--- bada-back/src/test/java/com/bada/badaback/auth/controller/AuthApiControllerTest.java
+++ bada-back/src/test/java/com/bada/badaback/auth/controller/AuthApiControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d20a62f229bc1406e8be3d333cc3d0f6c355d51d4e4680ebae21813cc54a8cdd
-size 4059
+oid sha256:c25cf1c82cb78042bcb349cfc757a72f7469c3c6cc161ce690c85310cee06dbc
+size 5232
