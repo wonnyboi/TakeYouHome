e468554713895993bb2a803a3ad28f7ae18d040e
sunkyoung <yskkjdh@gmail.com>
2024-03-16 11:55:38 +0900

    test : 부모 회원가입 관련 테스트 코드 수정 및 아이 회원가입 관련 테스트 코드 작성
    Change-Id: Ibc04503a5227841103533449375ae74dfea4a9a3
    
    Former-commit-id: 17497215990cd459304035d0d11276ff87157a20

--- bada-back/src/test/java/com/bada/badaback/auth/controller/AuthApiControllerTest.java
+++ bada-back/src/test/java/com/bada/badaback/auth/controller/AuthApiControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33b02ee3d83cc958d9b2bdb48ce92d292a87aee87a6cc069d13fc591fc2e665f
-size 4129
+oid sha256:d20a62f229bc1406e8be3d333cc3d0f6c355d51d4e4680ebae21813cc54a8cdd
+size 4059
