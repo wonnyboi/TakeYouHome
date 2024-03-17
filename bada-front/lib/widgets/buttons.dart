f4abab79c3d2bd2dad347add532df37bf5271e3f
정휘원 <wonnyboi@naver.com>
2024-03-17 23:23:04 +0900

    feat : 페이지 별 이동 방식 변경 알람 데이터 수신 방식 변경 내 가족 멤버 별 알림 및 설정 데이터 수신 추가
    Change-Id: I4b0ce14fc6f6b8f5ca01bee3a754c94921279099
    
    Former-commit-id: aad5d25cdba95d17bdec5d1787b6cb8445a89d39

--- bada-front/lib/widgets/buttons.dart
+++ bada-front/lib/widgets/buttons.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93001869277a472e044c6e7dac2abed20aece5936240e09d3a0906fbf457b87e
-size 9803
+oid sha256:780e3102ca70d61cbd290731fdbaa9bcfb80079c8d8d348c8d2e806c96dde7c8
+size 11993
