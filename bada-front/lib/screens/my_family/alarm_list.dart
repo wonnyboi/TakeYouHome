37ef40ed62da65d17daf34ef35e09d1d1e62a12f
정휘원 <wonnyboi@naver.com>
2024-03-17 23:23:04 +0900

    feat : 페이지 별 이동 방식 변경 알람 데이터 수신 방식 변경 내 가족 멤버 별 알림 및 설정 데이터 수신 추가
    Change-Id: I4b0ce14fc6f6b8f5ca01bee3a754c94921279099
    
    LGTM
    
    
    Former-commit-id: 3df743c699bec7c95a2f183bcfb06cacf3e941f5

--- bada-front/lib/screens/my_family/alarm_list.dart
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5a5a5710678be1ac8e62b72e55135891aeb9e68fc3c21a58d35e9cf8a91d895b
-size 1975
--- bada-front/lib/screens/my_family/fam_member.dart
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:40321236392ca1a63894e748ee3a8d2bc41c7f51ca4bf13d06cccda9e364e8eb
-size 2215
