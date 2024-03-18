4b8ab162beb2f3dd28e98c939cadce05bf451815
sunkyoung <yskkjdh@gmail.com>
2024-03-18 14:46:32 +0900

    test : memberService 가족코드 안받도록 수정
    Change-Id: I30bff0147951e19b02ddca62e685bc63fc78a7ac
    
    Former-commit-id: f94c378a2ce8a1bbcead9bcac03113dd3b1c8627

--- bada-back/src/test/java/com/bada/badaback/member/service/MemberListServiceTest.java
+++ bada-back/src/test/java/com/bada/badaback/member/service/MemberListServiceTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8358cecf52b6be4d0dda39e85f452689097f552b771f52e13bcdde61aa2c7fab
-size 2210
+oid sha256:9b19735df436431e88980296134755c1759608fe7c025fc2b1cb55e970fc198f
+size 1997
