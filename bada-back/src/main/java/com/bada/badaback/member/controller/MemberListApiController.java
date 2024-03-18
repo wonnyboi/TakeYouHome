b9d0e5ff84f9ee73afb2dbd573437a191c4eb60b
sunkyoung <yskkjdh@gmail.com>
2024-03-18 17:14:14 +0900

    refactor : 가족 조회 API 주소 수정
    Former-commit-id: 73cc3e76ce2ba1c3b40d64169ade13cc261c7411

--- bada-back/src/main/java/com/bada/badaback/member/controller/MemberListApiController.java
+++ bada-back/src/main/java/com/bada/badaback/member/controller/MemberListApiController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d9e87663a8170333b55e5385b78fb7fb9d0a8beaeeed838eaf12e48528cde53
-size 1048
+oid sha256:aa697082b2f1c3b0e237f7a96bd0e4eb264fb9c589ba5ea90275ca3790cc2d9e
+size 968
