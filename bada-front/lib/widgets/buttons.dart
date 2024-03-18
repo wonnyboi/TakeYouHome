ae1c980fa4131e06f6eadc8b53a548d52db5b541
정휘원 <wonnyboi@naver.com>
2024-03-18 14:41:29 +0900

    feat : 화면 구성 변경, 불필요 파일 삭제
    Change-Id: If05e94d350ae4c54e6cc33a374132b5866170cb2
    
    Former-commit-id: 884589c54535ce19b2cea0a8b5fe609bb000a48a

--- bada-front/lib/widgets/buttons.dart
+++ bada-front/lib/widgets/buttons.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:780e3102ca70d61cbd290731fdbaa9bcfb80079c8d8d348c8d2e806c96dde7c8
-size 11993
+oid sha256:e015f82c58ba13447d0d7e6034ce7d76019447553de285b4cd1f76b0f4cd9b96
+size 12164
