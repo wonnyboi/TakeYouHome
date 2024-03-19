83cda193e9c68b5d592895e27ac806f4c1c8f2dc
sunkyoung <yskkjdh@gmail.com>
2024-03-20 01:14:48 +0900

    refactor : 마이플레이스 등록에 지번 주소, 도로명 주소도 받도록 수정
    Change-Id: Iea35aa52053633317a6a81737969293269413945
    
    Former-commit-id: cc605607abdb52a0be6fac367f9d4e2d8b83ce38

--- bada-back/src/main/java/com/bada/badaback/myplace/service/MyPlaceService.java
+++ bada-back/src/main/java/com/bada/badaback/myplace/service/MyPlaceService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39829873bbe67f7a02a645d80304b3a6c1f79c1542afb6621755af049ffe0f1a
-size 2675
+oid sha256:0bf0cdb2f6a0926369f1362da9bf7e12a5a09edfa4c9a086b4acb9cb886d6236
+size 2749
