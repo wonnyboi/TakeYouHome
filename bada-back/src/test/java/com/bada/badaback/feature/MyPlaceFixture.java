09a705acacf65bebc3d40f24907d84ee8858ee6b
sunkyoung <yskkjdh@gmail.com>
2024-03-19 14:15:57 +0900

    test : 마이플레이스 등록, 수정, 삭제 관련 테스트 코드 작성
    Change-Id: I7ec6607c5526209e6b12b6b482c4928b457d91f8
    
    Former-commit-id: 0c7670717bd26ba372cd4401b3a0ef39fb3fac12

---
+++ bada-back/src/test/java/com/bada/badaback/feature/MyPlaceFixture.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1b653958b063ecd45b5354cc0c6c153948b7a9f3a30c6541ce20b1cdf44b49a3
+size 827
---
+++ bada-back/src/test/java/com/bada/badaback/myplace/service/MyPlaceServiceTest.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c0f7ff7d83009a192a54bb2a09d8b2356c728bddcd94f2889de88ede268733bb
+size 3489
