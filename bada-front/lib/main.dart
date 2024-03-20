73cdedaece57e4850e80057f7c1f0cddc00818ac
정휘원 <wonnyboi@naver.com>
2024-03-20 10:54:27 +0900

    bug : 화면 크기 적응형으로 변경중.
    Change-Id: Iadce77845a855fb1d302df2901bc42d580573711
    
    Former-commit-id: 8a7e8cb811ed9f67952d3cf021e09af3071f59d8

--- bada-front/lib/main.dart
+++ bada-front/lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce03ce9afacb97f6d40fcd491202009e597dbcc5bf2e4c5dc95d1cc95c6a8f50
-size 2615
+oid sha256:21ab26b182c23f405f4fa0fef57680d51cce21b56c1988459be39fa946e5fbd6
+size 2843
--- bada-front/lib/provider/profile_provider.dart
+++ bada-front/lib/provider/profile_provider.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ddc41849bccaee68a0759e9d350daaa01e47e008decff1b61d9022592c8ff62e
-size 2175
+oid sha256:5f7b106b350d3748740a7246c43b6cd40bcc27a80526804ad2ce511b85c15993
+size 5361
