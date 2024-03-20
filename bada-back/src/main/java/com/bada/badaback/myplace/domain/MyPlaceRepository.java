82228ed66f92add7904ab4f4c95b762b724b7f80
sunkyoung <yskkjdh@gmail.com>
2024-03-20 13:26:38 +0900

    feat : 마이플레이스 상세조회 및 전체조회 API 구현
    Change-Id: Ia501082893ad070a3ff034bf5059e1304132a44b
    
    Former-commit-id: 11101fd463abc8c7eba248326040ddf576891bcb

--- bada-back/src/main/java/com/bada/badaback/myplace/domain/MyPlaceRepository.java
+++ bada-back/src/main/java/com/bada/badaback/myplace/domain/MyPlaceRepository.java
@@ -1,6 +1,12 @@
 package com.bada.badaback.myplace.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MyPlaceRepository extends JpaRepository<MyPlace, Long> {
+    @Query("select distinct mp from MyPlace mp where mp.id in :placeIdList order by mp.createdAt desc")
+    List<MyPlace> myPlaceList(@Param("placeIdList") List<Long> placeIdList);
 }
