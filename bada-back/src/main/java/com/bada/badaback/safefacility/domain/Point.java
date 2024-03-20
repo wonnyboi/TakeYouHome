00a193ea670792809e8dc87ffd2d6ab0b8c513fb
sunkyoung <yskkjdh@gmail.com>
2024-03-20 14:04:27 +0900

    edit : 불필요한 import문 제거
    Former-commit-id: 028a420e305b2dc08a8f609c873e3707f72287b4

--- bada-back/src/main/java/com/bada/badaback/safefacility/domain/Point.java
+++ bada-back/src/main/java/com/bada/badaback/safefacility/domain/Point.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6fc372fbc02aaa780a68af02e968b0e27f69e17715c976f37c1002d7b3acb912
-size 261
+oid sha256:2acb5b867b3b3df9713ede28f9b39d128cdcaa559311f2865eb294dc2802dd44
+size 228
--- bada-back/src/main/java/com/bada/badaback/safefacility/dto/SafeFacilityRequestDto.java
+++ bada-back/src/main/java/com/bada/badaback/safefacility/dto/SafeFacilityRequestDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7c581c0fbf2974fb82ea632d5c468e9ef4da92c51f64f910b4697479129263e
-size 519
+oid sha256:889dc53f17125a36e991f48f52f7892cbcaceb43b3a3f8a4b599b40db1264b94
+size 495
