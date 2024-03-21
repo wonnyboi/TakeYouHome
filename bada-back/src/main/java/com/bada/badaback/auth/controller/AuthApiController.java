1f1b6fb996fe335597edf7a64359a48aeb437f23
sunkyoung <yskkjdh@gmail.com>
2024-03-21 13:17:06 +0900

    refactor : /api/auth 기존회원인증 get -> post로 변경
    Change-Id: Ib980108d8c3f8e5c79edea1f95564ee502383321
    
    Former-commit-id: b18208c72a1e7415986c3b4f68ad2b9802da93f7

--- bada-back/src/main/java/com/bada/badaback/auth/controller/AuthApiController.java
+++ bada-back/src/main/java/com/bada/badaback/auth/controller/AuthApiController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b8da76f400bf2588e787bb2df5801f4d54ed7057ee209e81ef426081ebb6677
-size 2352
+oid sha256:12cc4847ce1bb936676d18d01cb7702fd072584944af6262143aa7c463edb289
+size 2353
