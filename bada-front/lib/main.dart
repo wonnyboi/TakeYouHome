364340a0d67530e8c00106f90168a4d06e6c7ac5
정휘원 <wonnyboi@naver.com>
2024-03-21 14:34:40 +0900

    fixed merging
    Change-Id: I4a7590bdb5c93f2bef8747a06cac3dd72c5b51f8
    
    Former-commit-id: 0f0741bb02a3501d6421d66fd30b5900453d3a82

--- bada-front/lib/main.dart
+++ bada-front/lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d797b3f3ca0007eca21e9e88eea5b36f1e07f67bc93611a48d74fdf20949b8d6
-size 2793
+oid sha256:c945a810e2e828c7a9c520a889c8d86f23c2949f385207b47b45e03c08488daa
+size 2690
