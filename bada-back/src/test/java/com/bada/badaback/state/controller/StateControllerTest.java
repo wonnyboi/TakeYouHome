6b69a91888fbdff0e544fecace2c53a1b2d72cb1
SIMJIYEON <sim.yyeon@gmail.com>
2024-03-21 16:33:32 +0900

    test: 상태 api 테스트코드 작성
    Former-commit-id: cef64f8479647f815bb22113bc7cc3b54251c5ed

---
+++ bada-back/src/test/java/com/bada/badaback/state/controller/StateControllerTest.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:54d6a3f44a8c41f6bd554016a965e22a4566508d3d41c95657f28e7db0b40363
+size 10152
---
+++ bada-back/src/test/java/com/bada/badaback/state/service/StateServiceTest.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:969cccbd5594b094f81b4733833daef8326c2550c7ed80eb3dd832088f9bc708
+size 4967
