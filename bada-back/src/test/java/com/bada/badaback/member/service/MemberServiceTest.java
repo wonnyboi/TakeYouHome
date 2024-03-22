b16ecb86fe87cc653684e13da76dc35cf0bc0aaf
sunkyoung <yskkjdh@gmail.com>
2024-03-22 13:33:13 +0900

    test : 회원탈퇴 API 관련 테스트 코드 작성
    Change-Id: I08e608f36fb43bc3c229ddbd5261f35a2347d31d
    
    Former-commit-id: ebc6013603d9eacddafece1768301fb4dd798668

--- bada-back/src/test/java/com/bada/badaback/member/service/MemberServiceTest.java
+++ bada-back/src/test/java/com/bada/badaback/member/service/MemberServiceTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8946df0e2c7c83ea2a27d5af8079a978696fd89d3ec991031aa3712669cff5c5
-size 2899
+oid sha256:4df0ae75855f0b8023592a0fead81c49a7eb1f978d38ee8e4fb5bd9e9cdb3a1e
+size 4762
