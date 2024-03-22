da5d1d53d592e5fc7db23b8345e40187867f5ca1
sunkyoung <yskkjdh@gmail.com>
2024-03-22 13:31:05 +0900

    feat : StateRepository에 existsByMember 메서드 추가
    Change-Id: I37d9fcadc62ca0130902262935eb85d2494bfb57
    
    Former-commit-id: 958cef1af788e836a1c2af290cab3ae65f4257b4

--- bada-back/src/main/java/com/bada/badaback/state/domain/StateRepository.java
+++ bada-back/src/main/java/com/bada/badaback/state/domain/StateRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e7f36de133e679cc7674dc1cf792be5230246dd8fc1856815e46bb1a4fa0549
-size 299
+oid sha256:fdf0283c01154e9f1126a90020796db9a45b420e1110b567315815bfdaaf08b5
+size 343
