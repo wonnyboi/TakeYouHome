23a3f0b9b51d6152b07d283fc155e68e66ecf491
sunkyoung <yskkjdh@gmail.com>
2024-03-24 18:26:10 +0900

    refactor : inline variable 및 append 분리해서 call() 하도록 수정
    Change-Id: Ic7590eda33496f4f152691879d75477e59aa902a
    
    Former-commit-id: 7cdbf0749d95e9f7a3d84864b523bf9cfc987da9

--- bada-back/src/main/java/com/bada/badaback/safefacility/service/SafeFacilityService.java
+++ bada-back/src/main/java/com/bada/badaback/safefacility/service/SafeFacilityService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64da159ff03e6a8197fd57fdc7a1f60f2ed817f1ef1d506bcf810c39571d0c39
-size 5724
+oid sha256:413efa9f79a5bcedb7e986134ab9da4148c1a4a1718d542e474ec3e15a035cec
+size 5610
