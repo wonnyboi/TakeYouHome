f4543837ab7703df983885ce1dfa5a888a496288
SIMJIYEON <sim.yyeon@gmail.com>
2024-03-24 11:16:12 +0900

    refactor: path api post로 변경
    Change-Id: I2ab1358fefe72146e697057806e345f0c54a513a
    
    Former-commit-id: 6bd43cc119b70640e45f2e89f1acbb0a72120086

--- bada-back/src/main/java/com/bada/badaback/safefacility/controller/SafeFacilityController.java
+++ bada-back/src/main/java/com/bada/badaback/safefacility/controller/SafeFacilityController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80e40c018a8d76577607bfd273eee569012222d51b311eec8349bab67c33c528
-size 1695
+oid sha256:9c92cede2d3429e4bb0ee9228749354e1dfe68e47cc1817ee86f299d72824e74
+size 1697
