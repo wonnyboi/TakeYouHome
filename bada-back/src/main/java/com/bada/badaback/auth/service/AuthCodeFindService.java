68218b4e92e16bd84860a7e6f6ab1b5abb76638f
sunkyoung <yskkjdh@gmail.com>
2024-03-25 13:26:02 +0900

    refactor: 인증코드를 회원 못 찾을 시 나오는 errorCode 변경
    Change-Id: I06cbee7b661faadd65448f2b8308495a54c59d32
    
    Former-commit-id: c4b850fc37d475c46b3b785e5d3a9ba29bf6fe66

--- bada-back/src/main/java/com/bada/badaback/auth/service/AuthCodeFindService.java
+++ bada-back/src/main/java/com/bada/badaback/auth/service/AuthCodeFindService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7831ebf511fbfb38fc3ece5447dcb7372fae3d3326cd5240241302dfe74a0e68
-size 1086
+oid sha256:f85410583f9d79177d8bd51de1ed09dc67af3c59a050c674ac6a6b98840a7f03
+size 1097
