c506f0c0ead2a6e3244de3705f1454980af18bb9
SIMJIYEON <sim.yyeon@gmail.com>
2024-03-25 14:12:15 +0900

    test: 건물 좌표를 double로 테스트 코드 수정
    1. responsedto 생성할 때 double로 parse
    2. responsedto와 requestdto 비교할 때 double로 parse
    
    Change-Id: Ieb4a24a0cfbc0a516f5d50ce6e8f2c1457e19f69
    
    Former-commit-id: 989c1e7cc2a7cdef0613ddad88e9f807da3cb370

--- bada-back/src/test/java/com/bada/badaback/myplace/controller/MyPlaceApiControllerTest.java
+++ bada-back/src/test/java/com/bada/badaback/myplace/controller/MyPlaceApiControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89007efcd6d967d4fc88e1042f3ff28a908f471c36e54c79fb923c4664d0f7f7
-size 10649
+oid sha256:91ac068df52f50cc8e2490ed59e104ebbb94206f6e47aed3a8b47d8ac84d67db
+size 10645
