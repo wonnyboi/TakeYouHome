7a4207fa68722435072651d780250800bdb71ba7
sunkyoung <yskkjdh@gmail.com>
2024-03-25 10:43:03 +0900

    feat: myplace에 컬럼 placeCategoryName 추가 및 마이플레이스 삭제 서비스 메서드 오류 해결
    Change-Id: I1e63156ad3635ac281708c078ef0aa36cd8b24a4
    
    Former-commit-id: 3a30df5ae6f6bf27d9fdf19967a73245f051782c

--- bada-back/src/main/java/com/bada/badaback/myplace/controller/MyPlaceApiController.java
+++ bada-back/src/main/java/com/bada/badaback/myplace/controller/MyPlaceApiController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:639bfca50e61e9486b7346801a227bcf812bfa46cd1e43a50b13ac5734781ce0
-size 2318
+oid sha256:8ed1051d97232171b009d184558f7bbef1d97ac6b1be7a8af5aa97281e7e94d7
+size 2390
