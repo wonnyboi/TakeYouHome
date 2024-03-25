1cd7c4ed8d80b936cb463b81a69afda1b112ce7e
sunkyoung <yskkjdh@gmail.com>
2024-03-25 13:30:11 +0900

    edit : 불필요한 import문 및 ; 제거
    Change-Id: I0600e16c3b824f520f9d16c693a75e42cf2f59be
    
    Former-commit-id: b0f45a1b93e92d729fb8f8085548ba21c4a71542

--- bada-back/src/test/java/com/bada/badaback/member/domain/MemberRepositoryTest.java
+++ bada-back/src/test/java/com/bada/badaback/member/domain/MemberRepositoryTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d861bb9af1c2b4e33997e1d6c491971aa60c1c16f00d11b5dee2f172461f1db7
-size 5525
+oid sha256:c70bc0977a41f105f9ceedbc808e04dda99f0b808563f982235690c0ebb1db55
+size 5523
