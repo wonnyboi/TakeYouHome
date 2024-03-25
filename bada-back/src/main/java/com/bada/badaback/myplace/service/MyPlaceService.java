49896302971dcbf9798b11577d031e2574a58808
sunkyoung <yskkjdh@gmail.com>
2024-03-25 10:52:12 +0900

    refactor: myplace 삭제 에서 family placeList 수정 로직 분리
    Change-Id: Ic1b78eb2303af033b2f12918ed7296bd98e904be
    
    Former-commit-id: d11eba8aef4d62aa16a9949126e8ff477f500c86

--- bada-back/src/main/java/com/bada/badaback/myplace/service/MyPlaceService.java
+++ bada-back/src/main/java/com/bada/badaback/myplace/service/MyPlaceService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:53ffce8afcc72fd475bfa9bc472403d66091d775f1c9549434147d0239374d0d
-size 2987
+oid sha256:7d49c1cf1b283c44543d2ed95844795dd6f6eaacf199c1a7d6498d763dc9931d
+size 2520
