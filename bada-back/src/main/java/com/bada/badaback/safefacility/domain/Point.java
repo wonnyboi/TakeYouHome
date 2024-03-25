027fb8f62daf0f18d1668c5840e5e3db612ff162
SIMJIYEON <sim.yyeon@gmail.com>
2024-03-25 15:18:37 +0900

    feat: 추천 api 경유지 선정 완료
    Change-Id: I151e24dfe423fdc6c0730d3886b0229a8692d29f
    
    Former-commit-id: fd07ec9ad1a34df7f1b08090d0b6ea9dbebddd3b

--- bada-back/src/main/java/com/bada/badaback/safefacility/domain/Point.java
+++ bada-back/src/main/java/com/bada/badaback/safefacility/domain/Point.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2acb5b867b3b3df9713ede28f9b39d128cdcaa559311f2865eb294dc2802dd44
-size 228
+oid sha256:1a086a2eba44817202d72608d506ec0caa721096f27c1c6cae466646ef832fdb
+size 358
--- bada-back/src/main/java/com/bada/badaback/safefacility/service/SafeFacilityService.java
+++ bada-back/src/main/java/com/bada/badaback/safefacility/service/SafeFacilityService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e07f60c866b1200de59f1f237d8d7ddfabece37ab2b0e3122102cd1437d75825
-size 4384
+oid sha256:344a6d975b72ba105a68b086df67681318da7345bde4bf375c672a82646bf1fe
+size 12314
