0900efe3eb4f907707ccb5c75cce3081a8659777
SIMJIYEON <sim.yyeon@gmail.com>
2024-03-25 15:18:17 +0900

    test: 경로추천 api 중간 경유지 선정 테스트 코드 수정
    Change-Id: Ica1bd0a9c8662ddef6d8a44acd32caf5b45ec04f
    
    Former-commit-id: 8a56a77fd694c40372b7e6e34f5ef29a334aa877

--- bada-back/src/main/java/com/bada/badaback/safefacility/domain/Point.java
+++ bada-back/src/main/java/com/bada/badaback/safefacility/domain/Point.java
@@ -1,14 +1,3 @@
-package com.bada.badaback.safefacility.domain;
-
-import lombok.Builder;
-import lombok.Getter;
-import lombok.ToString;
-
-@Getter
-@Builder
-@ToString
-public class Point {
-    private Double longitude;
-    private Double latitude;
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:2acb5b867b3b3df9713ede28f9b39d128cdcaa559311f2865eb294dc2802dd44
+size 228
