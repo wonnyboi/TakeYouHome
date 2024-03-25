a7b302ec08b9ba1fbec3340a2cfa7798edb8e91c
이창헌 <changbill@naver.com>
2024-03-25 15:03:20 +0900

    feat : 가족 리스트 불러오기 api 수정, member model 수정
    Change-Id: Ib20c5113f4c1f5d859b4c5e8715fcdd02cf3f69e
    
    Former-commit-id: 54568aff76972471449fcaaf35255e92e457fda6

--- bada_kids_front/lib/model/member.dart
+++ bada_kids_front/lib/model/member.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b70431f2bd9e9be4a57830eb00eb7155b71507b2913951219f8f339fb1501abd
-size 1314
+oid sha256:2f56181931e4ccbe22aca41968a3df756cfa7c1dd8b63557c13f3aa5283b80b5
+size 1305
