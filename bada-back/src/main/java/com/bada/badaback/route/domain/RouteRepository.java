959616699f1c9959ca97d4c19ea1492b6a14655f
SIMJIYEON <sim.yyeon@gmail.com>
2024-03-26 22:53:59 +0900

    feat: 경로 api 설계
    Former-commit-id: 015c134d839b515265e747bc0ef39cb40bb12a5f

---
+++ bada-back/src/main/java/com/bada/badaback/route/domain/RouteRepository.java
@@ -0,0 +1,10 @@
+package com.bada.badaback.route.domain;
+
+import com.bada.badaback.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RouteRepository extends JpaRepository<Route,Long> {
+    Optional<Route> findByMember(Member member);
+}
---
+++ bada-back/src/main/java/com/bada/badaback/route/dto/RouteRequestDto.java
@@ -0,0 +1,15 @@
+package com.bada.badaback.route.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record RouteRequestDto(
+        @NotBlank(message = "출발 latitude는 필수입니다.")
+        String startLat,
+        @NotBlank(message = "출발 longitude는 필수입니다.")
+        String startLng,
+        @NotBlank(message = "도착 latitude는 필수입니다.")
+        String endLat,
+        @NotBlank(message = "도착 longitude는 필수입니다.")
+        String endLng
+) {
+}
