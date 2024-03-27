4bc177326d682cbff505ded77c26a9815a92f731
이용준 <sark2@naver.com>
2024-03-27 15:23:11 +0900

    edit: AlarmLog 엔티티 수정 및 알림 기록 조회 변경
    Change-Id: I042b077c4d26b73c5cb9200cbb2a7ffb8b278de5
    
    Former-commit-id: 4dd76e667c8bb28beec32bc886a9c19d3466c4aa

--- bada-back/src/main/java/com/bada/badaback/kafka/service/EmitterService.java
+++ bada-back/src/main/java/com/bada/badaback/kafka/service/EmitterService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f8aaed18fe05409734127fba3999851ac52898c6ef4cbbe0596c4d835131b50
-size 4449
+oid sha256:782c69d212018859a2829c1f2f82a31f04354e7cc84e163c4decd129219e008e
+size 4289
