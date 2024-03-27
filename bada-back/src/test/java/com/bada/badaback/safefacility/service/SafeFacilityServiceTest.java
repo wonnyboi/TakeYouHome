661109ae954b102b335de676bf0b37b055a496ab
SIMJIYEON <sim.yyeon@gmail.com>
2024-03-27 17:17:25 +0900

    test: SafeFacilityServiceTest 주석 처리
    Former-commit-id: 19d1abdb8b3dbd95e8eee5ba99930253e3f1e337

--- bada-back/src/test/java/com/bada/badaback/safefacility/service/SafeFacilityServiceTest.java
+++ bada-back/src/test/java/com/bada/badaback/safefacility/service/SafeFacilityServiceTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dac9b625147259bfb3a2e81737648800f12bb79be28c2f9427e7578c2917e37e
-size 2579
+oid sha256:b207c99e8a1c93f9a6c0ad899b77ac88d5efddd26d8ee6d14bfcf42dc82a288c
+size 2582
