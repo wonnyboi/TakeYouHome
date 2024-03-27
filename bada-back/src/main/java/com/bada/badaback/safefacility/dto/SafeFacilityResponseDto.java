ce30255ac10ebc5df004c301c8d24ac12465fe63
SIMJIYEON <sim.yyeon@gmail.com>
2024-03-27 17:01:41 +0900

    fix: ResponseDto pointList로 수정
    Former-commit-id: e1e024a9bf659ea697af87a254d31b932725d55f

--- bada-back/src/main/java/com/bada/badaback/safefacility/dto/SafeFacilityResponseDto.java
+++ bada-back/src/main/java/com/bada/badaback/safefacility/dto/SafeFacilityResponseDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e33067acd95f4318b1c13c030c7a79ae095e3a7e88f364f2a01779567ceffec5
-size 747
+oid sha256:fa1ffd2e998005b4da00f8e76acfa4ce72f68fdd8454330756575e4cf21e1345
+size 785
