3f6c5c7ce91773c37a9b2d458f6116173d9bbbed
SIMJIYEON <sim.yyeon@gmail.com>
2024-03-27 22:41:34 +0900

    feat: route api에서 pointList 전송 값 수정
    Former-commit-id: bd5be8cab0e3e1dcfb2aa99ce7e6ae53e01ec880

--- bada-back/src/main/java/com/bada/badaback/route/service/RouteService.java
+++ bada-back/src/main/java/com/bada/badaback/route/service/RouteService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:026c14bce039cf5f2535b069b135c8cec0c3147846569ae5abbb81ac6571c8a4
-size 2537
+oid sha256:c0b1dbc11c84863b7f9d1eb16721b83b60aaa7c3eaddce33d5034d14f995ad2d
+size 3289
