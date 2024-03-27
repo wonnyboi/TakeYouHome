96fd8e09dbf690615f2016e650e0bcf2f8199e16
정휘원 <wonnyboi@naver.com>
2024-03-27 09:36:01 +0900

    fixing git
    Former-commit-id: 7f547c5dbf797ceca63442b215a13a3df6e8e5aa

--- bada-front/lib/login/login_platform.dart
+++ bada-front/lib/login/login_platform.dart
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ee6c60f105d6d9ccd7dad675aa2e970248cd7e7716eb544a794072cf171b0b31
-size 59
+enum LoginPlatform {
+  kakao,
+  naver,
+  none, // logout
+}
