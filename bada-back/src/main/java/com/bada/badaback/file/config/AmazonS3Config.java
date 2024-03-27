dbb942c32833aea6389c8b47403c77dbcf5a13a6
SIMJIYEON <sim.yyeon@gmail.com>
2024-03-27 22:42:42 +0900

    edit: s3 설정 변경
    Former-commit-id: 6f97a82891606fa825d7cbbfb19d3f12efd7dedf

--- bada-back/src/main/java/com/bada/badaback/file/config/AmazonS3Config.java
+++ bada-back/src/main/java/com/bada/badaback/file/config/AmazonS3Config.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90b534ddd88f9c30d68d4baa567d9bc8a3f5860a45db63041cccb60c50d6f4ea
-size 1083
+oid sha256:00ad4cbd6c9032b6f01375d2707f3096d1c0b8433d3787646e2c517c587e8af1
+size 1038
