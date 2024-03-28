c4860aedcb9e2cc82ff968809a7a1d86d3085c5a
이창헌 <changbill@naver.com>
2024-03-28 17:48:19 +0900

    feat : member_api 파일 수정
    Change-Id: Ib9ea5d425b231e0b728dab990504c40d740cd093
    
    Former-commit-id: 36ae1baf59fd1e1adb042103eaabbb0b51145e62

--- bada-front/lib/functions/router_config.dart
+++ bada-front/lib/functions/router_config.dart
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8876dbfae8e7cfc9de5eccac5e19d8d2948ccaf6d1f023eb1720cdc57a88a4f2
-size 273
+// lib/config/router_config.dart
+import 'package:bada/loading_screen.dart';
+import 'package:go_router/go_router.dart';
+
+final routerConfig = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/loading',
+      builder: (context, state) => const LoadingScreen(),
+    ),
+  ],
+);
