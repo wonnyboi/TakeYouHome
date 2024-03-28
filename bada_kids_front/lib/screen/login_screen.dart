87c9ff1cb1090a998544f4d6e598c47cc87a4820
정휘원 <wonnyboi@naver.com>
2024-03-28 14:05:24 +0900

    testing alarm
    Former-commit-id: 5b8fefb3dad7e3d406f164af2f2c863dd1c71180

--- bada_kids_front/lib/screen/login_screen.dart
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9866b2bdf3dcf3e230a64e7b889c33823a585ae71623f703a920725630438680
-size 2687
---
+++ bada_kids_front/lib/widget/fam_member.dart
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7b76bf1401de1b867b2e228a025691e036de96dcfc1d2d16d45c959ca9eed937
+size 2721
