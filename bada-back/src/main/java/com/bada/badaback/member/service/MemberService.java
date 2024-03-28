1907dd35dc12c4db827f7e60397f50d68a2b0770
sunkyoung <yskkjdh@gmail.com>
2024-03-28 15:49:22 +0900

    feat : updateMovingState() 0->1, 1->0이 아니라 현재 위치 생성하면 1로, 삭제할 때는 0으로 무조건 변경하도록 수정
    Former-commit-id: df9afff6ec65ea2f1e5d9cb5d85cd02b2b1daa85

--- bada-back/src/main/java/com/bada/badaback/member/service/MemberService.java
+++ bada-back/src/main/java/com/bada/badaback/member/service/MemberService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e7a69259ade9650ba37eab3583f6b6f08ff960ab75b82a4e4399ed47a1dd023
-size 3209
+oid sha256:6214ddc78bbf23812ad7173708a6152b82b2837f17d22faf7154521962c81735
+size 3236
