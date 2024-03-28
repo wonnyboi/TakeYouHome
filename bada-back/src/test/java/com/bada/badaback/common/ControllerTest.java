dc19f1731dea43edd3f8fec247821fc996ac486e
SIMJIYEON <sim.yyeon@gmail.com>
2024-03-28 16:43:14 +0900

    test: 경로 검색만 하는 api 테스트 코드
    Former-commit-id: cc473eede65fda4b540e02d20a29464cbb6ad87c

--- bada-back/src/test/java/com/bada/badaback/common/ControllerTest.java
+++ bada-back/src/test/java/com/bada/badaback/common/ControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13fddb9726187e01ebd5d1071537e49086c65affa98cfde648d483a1fdfcadeb
-size 5463
+oid sha256:9791f0c121da2b96240fecdffdc8fa48c847fb8a16be6901a2cc579662db5d09
+size 5692
---
+++ bada-back/src/test/java/com/bada/badaback/route/service/RouteSearchServiceTest.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:454e258edfa851ccfa5cb910b78c775c40e19636f2135281cc83e4fb2d782d3d
+size 1530
