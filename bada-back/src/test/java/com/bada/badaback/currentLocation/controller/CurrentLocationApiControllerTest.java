2eea3d86f286067ab2d03299f0a41e7f58878bbc
sunkyoung <yskkjdh@gmail.com>
2024-03-28 15:47:12 +0900

    refactor : Route 삭제 시 같이 제거하기로 하면서 불필요해진 CurrentLocation delete API 삭제 및 관련 테스트 코드 삭제
    Former-commit-id: 93a8d78c99e2f220bb3abc44143d1a44e28c9224

--- bada-back/src/test/java/com/bada/badaback/currentLocation/controller/CurrentLocationApiControllerTest.java
+++ bada-back/src/test/java/com/bada/badaback/currentLocation/controller/CurrentLocationApiControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c898031d90a752bef142a7d80078c6897d35cf6f7596caaf75c1ca5a239276b
-size 10220
+oid sha256:68de64718abd5e7bd6fa28f26dfdc278cb3b5eb4ce79580d34ea1c79149700f2
+size 8335
