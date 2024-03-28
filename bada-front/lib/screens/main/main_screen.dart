748dd67e5301b82f8afee25e3b465e7c266dd719
정휘원 <wonnyboi@naver.com>
2024-03-28 14:25:53 +0900

    fixing git
    Former-commit-id: 395417e991ec26c03bf273b4af36182e0604d149

--- bada-front/lib/screens/main/main_screen.dart
+++ bada-front/lib/screens/main/main_screen.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1c2cd897e5ebc86269ab5a4014594971926ea15744c9e7938f11f1bcf4544e9
-size 5791
+oid sha256:c35426ae16bddea7b22a81c7f1d2917e31c5c905bb939bc6834a88fb4ad8b3cd
+size 5801
