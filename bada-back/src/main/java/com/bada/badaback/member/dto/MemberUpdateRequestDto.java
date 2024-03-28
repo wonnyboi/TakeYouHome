bf5d031c222cee317f23278c9bdc43d307b92d54
sunkyoung <yskkjdh@gmail.com>
2024-03-28 17:11:52 +0900

    feat : 기존 회원 정보 수정 API에 사용되는 requestDto에 childId 추가해서 아이 정보 수정 API 구현
    Change-Id: I9c2b529f0ac05eca238af837d159439a6bdf6296
    
    Former-commit-id: 19655e603de655d033eaabf922e185807208e290

--- bada-back/src/main/java/com/bada/badaback/member/dto/MemberUpdateRequestDto.java
+++ bada-back/src/main/java/com/bada/badaback/member/dto/MemberUpdateRequestDto.java
@@ -1,9 +1,3 @@
-package com.bada.badaback.member.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record MemberUpdateRequestDto(
-        @NotBlank(message = "이름은 필수입니다.")
-        String name
-) {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:197cad6a60004859df491b597dfed8ab6f0dd868587106f1f008cdee3a931ea0
+size 232
