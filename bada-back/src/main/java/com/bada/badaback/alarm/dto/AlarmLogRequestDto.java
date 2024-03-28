14ec6946fdb11efa6c79e98814fc03f8ab2b907d
SIMJIYEON <sim.yyeon@gmail.com>
2024-03-28 11:15:41 +0900

    feat: 경로를 포함한 일정 범위 확인 메서드 생성
    Former-commit-id: 1be8a47ca0e2ae5d6f195df517ce537968856af7

--- bada-back/src/main/java/com/bada/badaback/alarm/dto/AlarmLogRequestDto.java
+++ bada-back/src/main/java/com/bada/badaback/alarm/dto/AlarmLogRequestDto.java
@@ -1,17 +1,3 @@
-package com.bada.badaback.alarm.dto;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-@Getter
-@Builder
-@ToString
-@NoArgsConstructor
-@AllArgsConstructor
-public class AlarmLogRequestDto {
-  private String type;
-  private Long memberId;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:f33bd80b743b91f2e672077058331665f7a1da1aeabe28642da13bf3aa4d0b30
+size 325
