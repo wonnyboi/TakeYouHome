56e15a406b71462d439ddf000202d46480439377
sunkyoung <yskkjdh@gmail.com>
2024-03-28 17:09:23 +0900

    test : 잘못 작성되어 있었던 myPlace 수정 api 테스트 코드 수정
    Former-commit-id: 64780c4240ab95d8b1de771ca6e973dee442747a

--- bada-back/src/test/java/com/bada/badaback/myplace/controller/MyPlaceApiControllerTest.java
+++ bada-back/src/test/java/com/bada/badaback/myplace/controller/MyPlaceApiControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91ac068df52f50cc8e2490ed59e104ebbb94206f6e47aed3a8b47d8ac84d67db
-size 10645
+oid sha256:decfbf8a9801242a43659a3db13cde836de6ccaad35b0770b8e7dd01d49aa7cc
+size 10656
