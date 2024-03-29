1b01f6aa0e8db5219e896780ae1ae76a9d87a6ec
sunkyoung <yskkjdh@gmail.com>
2024-03-29 11:05:27 +0900

    refactor : MemberUpdateRequestDto childId 타입을 String으로 변경
    Change-Id: Idf7db4af0535475bf6bf095625b22330c9ad567a
    
    Former-commit-id: 7e637ccc1fb3badda7bb35fa152a923532f3835c

--- bada-back/src/main/java/com/bada/badaback/member/dto/MemberUpdateRequestDto.java
+++ bada-back/src/main/java/com/bada/badaback/member/dto/MemberUpdateRequestDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:197cad6a60004859df491b597dfed8ab6f0dd868587106f1f008cdee3a931ea0
-size 232
+oid sha256:f7475d3487f59af983240ad6175387d78be99eab05ee0790b4b5cdc01875c5fc
+size 234
