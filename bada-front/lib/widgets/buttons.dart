ad577f1c74112bbafb89c8882fb3c36b14957005
정휘원 <wonnyboi@naver.com>
2024-03-29 16:52:36 +0900

    design: 메인 화면 변경중
    Former-commit-id: 624a75a11ca8ab08a60efddd48d09e9b70fdc204

--- bada-front/lib/widgets/buttons.dart
+++ bada-front/lib/widgets/buttons.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c873ff0e74f7e1a7c8f8d03d1b2b471581c6aa18c9e89044658033ca66f05688
-size 14842
+oid sha256:078b1d15c5f62e765edefc2c625557afb5313d8eb14db5c732f9ff57c2c0fa8e
+size 16942
