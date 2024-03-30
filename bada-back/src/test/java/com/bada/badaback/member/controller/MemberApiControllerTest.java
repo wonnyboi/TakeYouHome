a3c74f4a0c30df76b213fea31e2765b693ad6e95
sunkyoung <yskkjdh@gmail.com>
2024-03-30 23:59:28 +0900

    edit : 불필요한 import문 제거
    Change-Id: I9e272338efb6e9fb06fc149b9a4a6d56103f5058
    
    Former-commit-id: d86c0a83d9799c07a4d64e04d2dbbdf7e4e84131

--- bada-back/src/test/java/com/bada/badaback/member/controller/MemberApiControllerTest.java
+++ bada-back/src/test/java/com/bada/badaback/member/controller/MemberApiControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb66f630458e47b0f93e409cc51e97edf721b50d73df2fd0b36036685d0b2648
-size 8196
+oid sha256:bb9526e2065450b9bd8f0d9bd49d3d08296bc70ca7ee1b7d0a376834221cb985
+size 8154
