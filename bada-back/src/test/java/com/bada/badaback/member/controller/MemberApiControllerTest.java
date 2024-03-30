13805718d3f1d92f3d27cdedd10b8a7b120e24ac
sunkyoung <yskkjdh@gmail.com>
2024-03-30 23:59:02 +0900

    refactor : 회원(부모,아이) 정보 수정 API로 넘어온 데이터를 @RequestParam으로 받도록 수정
    Change-Id: Ic8e7782c7b4cbe5efaaa1acea6db412ef91ccdba
    
    Former-commit-id: 3529d208524cbf140ffd17a2258a77f756f56677

--- bada-back/src/test/java/com/bada/badaback/member/controller/MemberApiControllerTest.java
+++ bada-back/src/test/java/com/bada/badaback/member/controller/MemberApiControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb9ea3a809e8a9d003b60af034bdfc53098059ace9f13f838716e8c8adb85a99
-size 8898
+oid sha256:cb66f630458e47b0f93e409cc51e97edf721b50d73df2fd0b36036685d0b2648
+size 8196
