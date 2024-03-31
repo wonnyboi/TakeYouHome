7472390659eaf89e6d3b9312957c2a7277046945
sunkyoung <yskkjdh@gmail.com>
2024-03-31 22:00:49 +0900

    test : routeController 관련 테스트 코드 수정
    Change-Id: I402456385fb172d3d79fbad3e30dabbc50e67699
    
    Former-commit-id: 8556dcf3d4a4fccfe8da131c11840837a89cd91b

--- bada-back/src/test/java/com/bada/badaback/route/controller/RouteControllerTest.java
+++ bada-back/src/test/java/com/bada/badaback/route/controller/RouteControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e6d1a8bf6777e72debc3a21191ec43e06e82cdb87039484159f4ff83bf4b869
-size 7816
+oid sha256:50af13a5998f5ff72672908cd3003457fc541c10560c59e98a639cd14b87228a
+size 7825
