b8b85dbda2972e67cec72bd72207e7bc6605ea3e
SIMJIYEON <sim.yyeon@gmail.com>
2024-04-01 00:09:17 +0900

    test: route 테스트 코드에 출발지, 도착지 이름 추가
    Former-commit-id: 7e4b11d8f32c73fc2e0af6290747a776853c0cc8

--- bada-back/src/test/java/com/bada/badaback/route/controller/RouteSearchControllerTest.java
+++ bada-back/src/test/java/com/bada/badaback/route/controller/RouteSearchControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4640af1be09143c0ef24d58841c674fabc37b7c66cf921c2222f384d1142511
-size 3759
+oid sha256:edbd5a014d55468999662bd0f2766e773bf053f7cf70aa569ab2291b1d376a43
+size 3848
