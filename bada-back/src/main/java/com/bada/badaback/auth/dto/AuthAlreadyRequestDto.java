2d343bf29915f724398f1b6642fd3db8190037e7
sunkyoung <yskkjdh@gmail.com>
2024-04-01 13:33:43 +0900

    feat : 회원가입말고 login() 하면updateFcmToken() 호출
    Change-Id: I9560b046e3edea9e547410a8201ededbe9149b98
    
    Former-commit-id: ac1abbb0f78d0008aca33b33782eade17ffa1155

--- bada-back/src/main/java/com/bada/badaback/auth/dto/AuthAlreadyRequestDto.java
+++ bada-back/src/main/java/com/bada/badaback/auth/dto/AuthAlreadyRequestDto.java
@@ -1,7 +1,3 @@
-package com.bada.badaback.auth.dto;
-
-public record AuthAlreadyRequestDto(
-        String email,
-        String social
-) {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:3899f1c9e0323715f2f8589035bcc8f988fea017e37865eb0ef2b11d20a35b3c
+size 373
--- bada-back/src/main/java/com/bada/badaback/auth/service/AuthService.java
+++ bada-back/src/main/java/com/bada/badaback/auth/service/AuthService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:558074ed1e92840c2c22db462b9bc989b13a10266263b699d5cc13f90ab5b82b
-size 6163
+oid sha256:debd8eac401a6681cb9d1f7838f87d1f014de7b6f18eeadc6caca9eb7426327c
+size 5789
