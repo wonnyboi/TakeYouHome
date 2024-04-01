12ff57a081c9b7d03fec058204901dc85dd384d0
정휘원 <wonnyboi@naver.com>
2024-04-01 17:28:54 +0900

    feat: 알람 수신 완료 1. Foreground 상태 디자인 1차 완료 2. background 및 앱 꺼짐 상태에서 알람 수신 시 앱 키기
    Former-commit-id: 695d1c73ac4076f32cefd5d498f5c6d0f881d35d

---
+++ bada-front/lib/models/alarm_model.dart
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:936bf2356252fc383c0b72a03b7febce3205caef17d6b54cee56355da133e9a7
+size 1686
--- bada_kids_front/lib/screen/login/login_screen.dart
+++ bada_kids_front/lib/screen/login/login_screen.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca20dcdacf3eec2af08ca3259f715a67001b714a4182e06bc4a670fadadd7d51
+oid sha256:72306dc8cd2ffae4c534dcd157bf6dd22d7746c970e2699fec7a08fbf7fbda56
 size 2693
