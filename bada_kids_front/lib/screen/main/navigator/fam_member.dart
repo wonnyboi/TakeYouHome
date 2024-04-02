a6a478ad9718a3c1b91c05bf20f3e974dd688227
이창헌 <changbill@naver.com>
2024-04-02 14:22:35 +0900

    feat : 디자인 수정
    Change-Id: I7ab0fccb2798ae1473df2016439c0ed0b2b99826
    
    Former-commit-id: 0f8a0dfedd62610c046fba0ff595dcb17e2530c9

--- bada_kids_front/lib/screen/main/navigator/fam_member.dart
+++ bada_kids_front/lib/screen/main/navigator/fam_member.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da82565de0ce2b4d96f063d1ea7c54d8aae4c482b8f8a03b1cf0578ee6923cb3
-size 7997
+oid sha256:3d470ba16a494200753d3021dbbb129f3082a9d76b01484e19b1665c113eda22
+size 7341
