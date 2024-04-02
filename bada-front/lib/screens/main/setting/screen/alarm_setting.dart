4b91d87277506a853b1eaac6dee849c9d38933e5
정휘원 <wonnyboi@naver.com>
2024-04-02 16:23:03 +0900

    bug: release 모드로 apk 다운로드 시, 내 장소에서 + 버튼 누르면 회색 화면 나옴 1. 경로 찾기 지도 안나옴 2. 내 장소에서 지도 안나옴 3. 설정 화면에서 핸드폰 기종에 따라 회색 화면이 나옴
    Former-commit-id: 0346162445a697685558336a21caee282f9ec3ac

--- bada-front/lib/screens/main/setting/screen/alarm_setting.dart
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:26e55111069950cc465f55d5e101ab3fe16f87d9976404c2560e47d1c429bec5
-size 847
--- bada-front/lib/screens/main/setting/settings.dart
+++ bada-front/lib/screens/main/setting/settings.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:052e2951c56705e0d9268f7e1cac250b53edf7a56e419f7d39c89d5bf7d01c04
-size 12229
+oid sha256:44eaefb71b18e57781fdb97c4fb1e6afa192588b1bc4a4a346eba5b6194d7ac7
+size 11584
