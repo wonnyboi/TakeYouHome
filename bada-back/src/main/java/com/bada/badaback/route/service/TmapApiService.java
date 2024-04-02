35f43cde2e6b2ea020b728c46944df255a30ea59
SIMJIYEON <sim.yyeon@gmail.com>
2024-04-02 16:32:47 +0900

    refactor: 경로 추천 알고리즘 layer 개수 조정으로 수정
    Former-commit-id: 0cd5f196c80ea5010044b0b0b58898974834b227

--- bada-back/src/main/java/com/bada/badaback/route/service/TmapApiService.java
+++ bada-back/src/main/java/com/bada/badaback/route/service/TmapApiService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d851079fa2a3d816a00d421863b9766ffdb51d6c2ef17eeaa4d7722b8714903
-size 4199
+oid sha256:9fd86b785da51e4b460aeb4f8cfda22ff15b8042c6d116b7ed5e3428c5da98e5
+size 5582
--- bada-back/src/main/java/com/bada/badaback/safefacility/service/SafeFacilityService.java
+++ bada-back/src/main/java/com/bada/badaback/safefacility/service/SafeFacilityService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64b802af539c2afc7bfbc6c21718f3f448eea7cf1bbdb7cff009cec4d2f8d0b7
-size 12835
+oid sha256:af585454d189e4f9d24914df6511738d139b634fd3a8088f3f6ef8c736693fc5
+size 14086
