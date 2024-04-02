a541b9f5f97838d3cd38db453d99a03932dd1efe
정휘원 <wonnyboi@naver.com>
2024-04-02 14:22:33 +0900

    feat: 알람 시간 순서 별 배치 및 onTap시 애니메이션 재생
    Former-commit-id: 42613cb796081fe6a2fecbecca2da4c87c017815

--- bada-front/lib/models/alarm_model.dart
+++ bada-front/lib/models/alarm_model.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:936bf2356252fc383c0b72a03b7febce3205caef17d6b54cee56355da133e9a7
-size 1686
+oid sha256:be0cc09c0c487a50a6aad5a1284ec76571a7026e5d0181fe2c9f5debfe0274c7
+size 2068
