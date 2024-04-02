43c150a06f3aed9120e524422f261c0bf1eb4495
SIMJIYEON <sim.yyeon@gmail.com>
2024-04-02 17:11:48 +0900

    refactor: 경로 저장 시 데이터 길이 조절 및 에러 처리
    Former-commit-id: db5f68b3a95df775059b5d9e0d6bb2145ae71277

--- bada-back/src/main/java/com/bada/badaback/route/service/RouteService.java
+++ bada-back/src/main/java/com/bada/badaback/route/service/RouteService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc506c2c17fb1acb0eef6cc88ad36abc0c349f2ba62f56c92377229524e2c0d7
-size 3621
+oid sha256:403af2fe9154947d3c5e3e55445fafa7b98d9aa6470bacdf068586a1b2de4325
+size 3786
