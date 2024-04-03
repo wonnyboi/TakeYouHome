e96adf9e8b0461e3e6c4f83654c1360029503591
정휘원 <wonnyboi@naver.com>
2024-04-03 23:07:25 +0900

    feat: 설명서 수정중
    Former-commit-id: 0498ca4679fa9506de9944ceffb2fcda66f1bc7e

--- bada-front/lib/screens/main/tutorial/tutorial-list.dart
+++ bada-front/lib/screens/main/tutorial/tutorial-list.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf06f12d6c7d129afbc97f566294602c6437b192420bf466aa36ef0a4d182d35
-size 4331
+oid sha256:5d2ddb3859984dd72ccca3108d0fea6197a787633cd5c0bb791ebfc3ddf77292
+size 4391
