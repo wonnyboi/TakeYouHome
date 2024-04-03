c5b1b17be06663f6b8a64e5449f915bcacc06163
이창헌 <changbill@naver.com>
2024-04-03 17:14:35 +0900

    feat : 아이 로그인 시 default 이미지 넣기, alarm Text 수정
    Change-Id: Ie0b55a4481ee58e37da0ee4e60c88f8cb2458595
    
    Former-commit-id: 04a8e62da13604be5fb94ea90453b5a8854a20f6

--- bada-front/lib/widgets/alarm.dart
+++ bada-front/lib/widgets/alarm.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26b5fa05ae1c4fd3155cf68004b88fb8ed0ee75b587ffc25da9c80851122d4a0
-size 7624
+oid sha256:de85f44b27bff60dbcb9f3d69ed03ca88c6dca2990fb051b94df675ce5f8010b
+size 9122
--- bada_kids_front/lib/screen/login/login_screen2.dart
+++ bada_kids_front/lib/screen/login/login_screen2.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55f7f22aef4f0c75dcd8c52297771675aadd9d4a13b44938b50a840f480ffc91
-size 7539
+oid sha256:131cff2516aa225d56097cc33ffdce280469cfcd84847930c24f5cb200e13fd5
+size 7664
