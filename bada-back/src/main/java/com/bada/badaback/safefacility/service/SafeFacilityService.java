d40938953e07299215be1a40886d81a6fd83e8ed
SIMJIYEON <sim.yyeon@gmail.com>
2024-04-03 15:30:14 +0900

    edit: passCCTV 주석 수정
    Former-commit-id: 1c10a60eb2a41b67a1acbe2571ef481683917e34

--- bada-back/src/main/java/com/bada/badaback/safefacility/service/SafeFacilityService.java
+++ bada-back/src/main/java/com/bada/badaback/safefacility/service/SafeFacilityService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af585454d189e4f9d24914df6511738d139b634fd3a8088f3f6ef8c736693fc5
-size 14086
+oid sha256:81edc34c15119baefd2d4fe35525041138548c65a613cae1056f5a5e5f66d89c
+size 14151
