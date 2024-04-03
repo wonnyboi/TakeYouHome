e564c33410ce62726aa155bc304dc8d9cb44cba2
sunkyoung <yskkjdh@gmail.com>
2024-04-03 16:22:14 +0900

    refactor : 현재위치 생성,수정 시간 받도록 수정
    Former-commit-id: 735eaf6428498d6d5c46fc52220cd9eb9e8c7a65

--- bada-back/src/main/java/com/bada/badaback/currentLocation/domain/CurrentLocation.java
+++ bada-back/src/main/java/com/bada/badaback/currentLocation/domain/CurrentLocation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a538839f0a00c9df25b406a1ea49e22d16360ba43c457173b3960141af288522
-size 1341
+oid sha256:f722b3bcc8bc2ec3c38ed249947b412187b925a835c51913cbecfb591e9e968f
+size 1412
