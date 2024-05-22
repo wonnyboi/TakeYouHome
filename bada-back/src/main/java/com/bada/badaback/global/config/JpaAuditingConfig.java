e4e5e05e3a1bc09dc539d664e8b747b9b0d97e10
윤선경 <yskkjdh@gmail.com>
2024-05-22 15:28:39 +0900

    docs : 실행화면 gif 추가
    Former-commit-id: a333ef8d510af2cb92361848853e4c0ff39a00ff

--- bada-back/src/main/java/com/bada/badaback/global/config/JpaAuditingConfig.java
+++ bada-back/src/main/java/com/bada/badaback/global/config/JpaAuditingConfig.java
@@ -1,3 +1,9 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:afc7a350d507f0be44ba04969a99b1771ebfee483a47a925f187132c56b6d4f9
-size 246
+package com.bada.badaback.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
--- bada-front/lib/social_login/login_platform.dart
+++ bada-front/lib/social_login/login_platform.dart
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:97774a4a745904c5a89107ea3cec09502b44b096bb575d6d6f78b38eb46058f4
-size 49
+enum LoginPlatform {
+  kakao,
+  naver,
+  none,
+}
